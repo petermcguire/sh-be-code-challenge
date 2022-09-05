105e513c24fc3464f497595da3b3772091ad0abf
petermcguire <peter.mcguire@spothero.com>
2022-09-05 15:43:05 -0700

    initial commit

---
+++ src/main/kotlin/io/gzmo/Application.kt
@@ -0,0 +1,12 @@
+package io.gzmo
+
+import io.ktor.server.application.*
+import io.gzmo.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+}
